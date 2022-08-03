9661c1dd50a96730ef43290dca5c54bec69e12b0
Denwalker <denwalker@mail.ru>
2022-08-03 17:45:29 +0300

    first commit

---
+++ JS_learning/js/01 start.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let number = 10.4;
+console.log(number / 0);
+
+const persone = "Den";
+
+let und;
+console.log(und);
+
+
+
+
+const obj = {
+    name: "Den",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
---
+++ JS_learning/js/02 const.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('OOps!')
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Are you 18?", "18");
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt("What's your name?",""); 
+answers[1] = prompt("Surname?",'');
+answers[2] = prompt("Age?",'');
+
+document.write(answers);
